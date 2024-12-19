03029242d89189a839f36fc72453984566abb5ff
Saúl Krujoski <saulkrujoski@gmail.com>
2024-12-18 23:05:17 -0300

    Se sube contenido de ejemplo para formas de recuperar datos de formularios y en qué formato se ven.

--- demos/form-recovery-format/scripts/inputs.js
+++ demos/form-recovery-format/scripts/inputs.js
@@ -1,17 +1,22 @@
-console.log("--> Bienvenidos a la clase 10: Eventos - Ejemplos <--");
+console.log("--> Eventos - Ejemplos de cómo recuperar datos de un formualrio y el formato en el que se expresan <--");
 
-// Aplicación de getElementById
-const form = document.getElementById("my-examples-form");
-console.log("--> Formulario", form);
+// Aplicación de getElementById para recorrer todos y cada uno de los elementos del formulario y ver los datos cargados
+/* const form = document.getElementById("my-examples-form");
+
 form.addEventListener("submit", (ev) => {
     ev.preventDefault();
-    ev.target.elements.forEach((input) => {
-        console.log(input.id. input.value);
-        
-    });
-    
-});
+    console.log("--> Formulario", { form: ev.target });
+    for (let index = 0; index < ev.target.children.length; index++) {
+        const child = ev.target[index];
+        console.log(child.id, {
+            value: child.value,
+            checked: child.checked
+        });
+    }
 
+}); */
+
+// Podemos acceder a cada elemento individualmente
 /* const textInput = document.getElementById("exampleInputText");
 console.log("--> Text input", textInput);
 
@@ -32,11 +37,9 @@ console.log("--> Aspect Tree input", spectTreeInput); */
 
 // Aplicación de querySelectorAll
 // Construiremos un proceso que añada una captura del cambio en los datos proporcionados, a todos los inputs del formualrio
-/* const formElements = document.querySelectorAll('input, select, textarea, button');
-console.log("--> Inputs del formulario", formElements);
+const formElements = document.querySelectorAll('input, select, textarea, button');
 
 formElements.forEach((input) => {
-    console.log("--> Elemento " + input.getAttribute("id"), input);
     input.addEventListener("change", (ev) => {
         console.log("--> Cambiaste el contenido de " + ev.target.id, {
             value: ev.target.value,
@@ -44,4 +47,4 @@ formElements.forEach((input) => {
         });
 
     })
-}) */
+})
